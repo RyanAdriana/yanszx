06e14828a57c05b458512939dbf6fe74e79f6867
RyanAdriana <141312778+RyanAdriana@users.noreply.github.com>
2023-08-31 14:32:07 +0700

    Add files via upload

---
+++ GasolineReceipt.java
@@ -0,0 +1,16 @@
+public class GasolineReceipt{
+    public static void main( String[] args) {
+	    System.out.println("+------------------------+");
+		System.out.println("|                        |");
+		System.out.println("|       RYAN STORE       |");
+		System.out.println("|                        |");
+		System.out.println("|    2023-08-31 9:18PM   |");
+		System.out.println("|                        |");
+		System.out.println("|  Kebab:         10.000 |");
+		System.out.println("|  Sosis:        $ 5.000 |");
+		System.out.println("|                        |");
+		System.out.println("|  Fuel total:   $ 15.00 |");
+		System.out.println("|						 |");
+		System.out.println("+------------------------+");
+	}
+}
